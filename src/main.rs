e4510d1d74cc615339b67b4dec451477a4f2171a
Andrii Konotop <konotop401@gmail.com>
2025-02-09 13:59:37 +0100

    feat: add echoes of reading functionality and rearrange

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
-use std::{collections::hash_map::Entry, io::Read, net::SocketAddrV4};
+use std::{
+    collections::hash_map::Entry,
+    io::{ErrorKind, Read},
+    net::SocketAddrV4,
+};
 
 use mini_tcp::{
     device::TunDevice,
-    tcb::{ConnectionError, SocketPair, Tcb},
+    tcb::{SocketPair, Tcb},
     TCBTable,
 };
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> std::io::Result<()> {
     let mut dev = TunDevice::new().unwrap();
     let mut tcb_table = TCBTable::new();
     let mut buf = [0u8; 1500]; // MTU
 
-    // What's the next step of the operation?
     loop {
         let amount = dev.read(&mut buf)?;
         let pkt = &buf[0..amount];
@@ -24,27 +27,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 continue;
             }
 
-            let iph_end: usize = (ipd.ihl() << 2).into(); // IP header is 4 words long
-            match etherparse::TcpHeaderSlice::from_slice(&pkt[iph_end..]) {
-                Ok(tcp) => {
-                    println!("TCP {} -> {}", tcp.source_port(), tcp.destination_port());
+            let tcp_offset: usize = (ipd.ihl() << 2).into(); // IP header is 4 words long
+            match etherparse::TcpHeaderSlice::from_slice(&pkt[tcp_offset..]) {
+                Ok(tcph) => {
+                    let data_offset: usize = (tcph.data_offset() << 2).into();
+                    let payload = &pkt[tcp_offset + data_offset..];
                     // Uniquely represents a connection
                     let sp = SocketPair {
-                        local: SocketAddrV4::new(dest, tcp.destination_port()),
-                        remote: SocketAddrV4::new(src, tcp.source_port()),
+                        local: SocketAddrV4::new(dest, tcph.destination_port()),
+                        remote: SocketAddrV4::new(src, tcph.source_port()),
                     };
+
                     match tcb_table.connections.entry(sp) {
                         Entry::Vacant(vacant) => {
                             let tcb = Tcb::new(sp);
-                            vacant.insert(tcb).poll(&mut dev, tcp)?
+                            vacant.insert(tcb).poll(&mut dev, tcph, payload)?
                         }
                         Entry::Occupied(mut occupied) => {
-                            if let Err(
-                                ConnectionError::ConnectionReset
-                                | ConnectionError::ConnectionRefused,
-                            ) = occupied.get_mut().poll(&mut dev, tcp)
-                            {
-                                tcb_table.connections.remove_entry(&sp);
+                            if let Err(error) = occupied.get_mut().poll(&mut dev, tcph, payload) {
+                                match error.kind() {
+                                    ErrorKind::ConnectionRefused | ErrorKind::ConnectionReset => {
+                                        println!("Removing a connection: {:?}", &sp);
+                                        tcb_table.connections.remove_entry(&sp);
+                                    }
+                                    _ => {}
+                                }
                             }
                         }
                     }
--- src/tcb.rs
+++ src/tcb.rs
@@ -1,16 +1,11 @@
-use std::{collections::VecDeque, fmt::Display, io::Write, net::SocketAddrV4};
+use std::{
+    collections::VecDeque,
+    fmt::Display,
+    io::{Error, ErrorKind, Write},
+    net::SocketAddrV4,
+};
 
 use crate::device::TunDevice;
-
-#[derive(thiserror::Error, Debug)]
-pub enum ConnectionError {
-    #[error("connection reset by peer")]
-    ConnectionReset,
-    #[error("connection refused")]
-    ConnectionRefused,
-}
-
-type TcbResult<T> = std::result::Result<T, ConnectionError>;
 
 #[derive(Hash, Eq, PartialEq, Debug)]
 pub enum State {
@@ -54,22 +49,44 @@ pub struct SocketPair {
     pub remote: SocketAddrV4,
 }
 
+#[derive(Hash, Eq, PartialEq, Debug)]
+pub enum ConnectionType {
+    Active,
+    Passive,
+}
+
 // Transmission Control Block
 #[derive(Hash, Eq, PartialEq, Debug)]
 pub struct Tcb {
     state: State,
     pair: SocketPair,
+    connection_type: ConnectionType,
+    /// Transmit buffer
     tx_buffer: VecDeque<u8>,
+    /// Receive buffer
     rx_buffer: VecDeque<u8>,
-    snd_una: u32,
-    snd_nxt: u32,
-    snd_wnd: u16,
-    snd_wl1: u32,
-    snd_wl2: u32,
+    /// Initial seq number of sender
     iss: u32,
-    rcv_nxt: u32,
-    rcv_wnd: u32,
+    /// Last unacknowledged byte sent
+    snd_una: u32,
+    /// Next seq number to be sent
+    snd_nxt: u32,
+    /// Available buffer space for sending
+    snd_wnd: u16,
+    /// Used for urgent data
+    snd_up: u32,
+    /// Last segment’s sequence number for window update
+    snd_wl1: u32,
+    /// Last segment’s acknowledgment number for window update
+    snd_wl2: u32,
+    /// Initial seq number of receiver
     irs: u32,
+    /// Next byte expected from sender
+    rcv_nxt: u32,
+    /// Available buffer space for receiving
+    rcv_wnd: u32,
+    /// Used for urgent data
+    rcv_up: u32,
 }
 
 impl Tcb {
@@ -77,73 +94,57 @@ impl Tcb {
         Self {
             pair,
             state: State::Listen,
+            connection_type: ConnectionType::Passive,
             tx_buffer: VecDeque::with_capacity(4096),
             rx_buffer: VecDeque::with_capacity(4096),
+            iss: rand::random::<u32>(),
             snd_una: 0,
             snd_nxt: 0,
             snd_wnd: 0,
             snd_wl1: 0,
             snd_wl2: 0,
-            iss: rand::random::<u32>(),
+            snd_up: 0,
+            irs: 0,
             rcv_nxt: 0,
             rcv_wnd: 0,
-            irs: 0,
+            rcv_up: 0,
         }
     }
 
-    pub fn poll(&mut self, dev: &mut TunDevice, seg: etherparse::TcpHeaderSlice) -> TcbResult<()> {
+    pub fn poll(
+        &mut self,
+        dev: &mut TunDevice,
+        seg: etherparse::TcpHeaderSlice,
+        payload: &[u8],
+    ) -> std::io::Result<()> {
         match self.state {
-            State::Listen => self.process_listen(dev, seg),
-            State::SynRcvd => self.process_syn_rcvd(dev, seg),
-            State::Estab => self.process_estab(dev, seg),
-            State::Closed => self.process_close(dev, seg),
-            State::SynSent => self.process_syn_sent(dev, seg),
+            State::Listen => self.listen(dev, seg),
+            State::SynRcvd => self.syn_rcvd(dev, seg),
+            State::Estab => self.estab(dev, seg, payload),
+            State::Closed => self.close(dev, seg),
+            State::SynSent => self.syn_sent(dev, seg),
+            State::CloseWait => Ok(()),
             _ => Ok(()),
         }
     }
 
-    pub fn send_datagram(
-        &self,
-        dev: &mut TunDevice,
-        tcph: etherparse::TcpHeader,
-        payload: &[u8],
-    ) -> std::io::Result<()> {
-        let builder = etherparse::PacketBuilder::ipv4(
-            self.pair.local.ip().octets(),
-            self.pair.remote.ip().octets(),
-            HOP_LIMIT,
-        )
-        .tcp_header(tcph);
-
-        let mut datagram = Vec::<u8>::with_capacity(builder.size(payload.len()));
-
-        match builder.write(&mut datagram, payload) {
-            Ok(_) => dev.write_all(datagram.as_slice()),
-            Err(_) => Err(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "Packet serialization failed",
-            )),
-        }
-    }
-
-    pub fn process_listen(
+    pub fn listen(
         &mut self,
         dev: &mut TunDevice,
         seg: etherparse::TcpHeaderSlice,
-    ) -> TcbResult<()> {
+    ) -> std::io::Result<()> {
         // RST should be ignored in LISTEN state
         if seg.rst() {
             return Ok(());
         }
         if seg.ack() {
-            self.send_rst(dev, seg.acknowledgment_number()).unwrap();
-            return Ok(());
+            self.send_rst(dev, seg.acknowledgment_number())?
         }
 
         // Security and precedence checks are skipped
         if seg.syn() {
-            self.rcv_nxt = seg.sequence_number() + 1;
             self.irs = seg.sequence_number();
+            self.rcv_nxt = seg.sequence_number().wrapping_add(1);
 
             let ws = self.window().unwrap();
             let mut tcph = etherparse::TcpHeader::new(
@@ -156,35 +157,73 @@ impl Tcb {
             tcph.syn = true;
             tcph.ack = true;
 
-            self.snd_nxt = self.iss + 1;
             self.snd_una = self.iss;
+            self.snd_nxt = self.iss.wrapping_add(1);
 
             self.state = State::SynRcvd;
 
-            self.send_datagram(dev, tcph, &[]).unwrap();
+            self.send_datagram(dev, tcph, &[])?
         }
         // It's not ok, actually
         Ok(())
     }
 
-    pub fn process_syn_sent(
+    pub fn syn_rcvd(
         &mut self,
         dev: &mut TunDevice,
         seg: etherparse::TcpHeaderSlice,
-    ) -> TcbResult<()> {
+    ) -> std::io::Result<()> {
+        if seg.rst() {
+            if self.connection_type == ConnectionType::Passive {
+                self.state = State::Listen;
+                return Ok(());
+            } else {
+                // TODO: The retrasmission queue should be flushed
+                let _ = self.tx_buffer.flush();
+                return Err(Error::from(ErrorKind::ConnectionReset));
+            }
+        }
+
+        if seg.ack() {
+            let seg_ack = seg.acknowledgment_number();
+            match seg_ack > self.snd_una && seg_ack <= self.snd_nxt {
+                true => {
+                    if seg.rst() {
+                        return Err(Error::from(ErrorKind::ConnectionReset));
+                    }
+                    self.state = State::Estab;
+                    println!("accepted a connection from: {}", self.pair.remote);
+
+                    // TODO: calculate window size precisely
+                    self.snd_wnd = seg.window_size();
+                    self.snd_wl1 = seg.sequence_number();
+                    self.snd_wl2 = seg.acknowledgment_number();
+                }
+                false => {
+                    self.send_rst(dev, seg.sequence_number())?;
+                }
+            }
+        }
+        Ok(())
+    }
+
+    pub fn syn_sent(
+        &mut self,
+        dev: &mut TunDevice,
+        seg: etherparse::TcpHeaderSlice,
+    ) -> std::io::Result<()> {
         let seg_ack = seg.acknowledgment_number();
         if seg_ack <= self.iss || seg_ack > self.snd_nxt {
             if seg.rst() {
                 return Ok(());
             }
-            self.send_rst(dev, seg_ack).unwrap();
+            self.send_rst(dev, seg_ack)?
         }
 
         match seg_ack >= self.snd_una && seg_ack <= self.snd_nxt {
             true => {
                 if seg.rst() {
-                    self.state = State::Closed;
-                    return Err(ConnectionError::ConnectionReset);
+                    return Err(Error::from(ErrorKind::ConnectionReset));
                 }
             }
             false => return Ok(()),
@@ -210,43 +249,7 @@ impl Tcb {
                 tcph.acknowledgment_number = self.rcv_nxt;
                 tcph.ack = true;
 
-                let _ = self.send_datagram(dev, tcph, &[]);
-            }
-        }
-        Ok(())
-    }
-
-    pub fn process_syn_rcvd(
-        &mut self,
-        dev: &mut TunDevice,
-        seg: etherparse::TcpHeaderSlice,
-    ) -> TcbResult<()> {
-        if seg.rst() {
-            // Only in case of passive open
-            self.state = State::Listen;
-            return Ok(());
-            // Active open may appear here only in case
-            // of simultaneous open. The retransmission
-            // queue should be flushed.
-            // Delete the TCB.
-        }
-
-        if seg.ack() {
-            let seg_ack = seg.acknowledgment_number();
-            match seg_ack > self.snd_una && seg_ack <= self.snd_nxt {
-                true => {
-                    if seg.rst() {
-                        self.state = State::Closed;
-                        return Err(ConnectionError::ConnectionReset);
-                    }
-                    self.state = State::Estab;
-                    self.snd_wnd = seg.window_size();
-                    self.snd_wl1 = seg.sequence_number();
-                    self.snd_wl2 = seg.acknowledgment_number();
-                }
-                false => {
-                    self.send_rst(dev, seg.sequence_number()).unwrap();
-                }
+                self.send_datagram(dev, tcph, &[])?
             }
         }
         Ok(())
@@ -256,6 +259,7 @@ impl Tcb {
         let mut tcph =
             etherparse::TcpHeader::new(self.pair.local.port(), self.pair.remote.port(), seq, 0);
         tcph.rst = true;
+
         self.send_datagram(dev, tcph, &[])
     }
 
@@ -266,9 +270,10 @@ impl Tcb {
     ) -> std::io::Result<()> {
         // <SEQ=0><ACK=SEG.SEQ+SEG.LEN><CTL=RST,ACK>
         let seg_len: u32 = seg.slice().len().try_into().unwrap();
-        // let seg_len: u32 = seg.to_header().header_len().try_into().unwrap();
+
         let mut tcph =
             etherparse::TcpHeader::new(self.pair.local.port(), self.pair.remote.port(), 0, 0);
+
         tcph.acknowledgment_number = seg.sequence_number() + seg_len;
         tcph.rst = true;
         tcph.ack = true;
@@ -276,64 +281,88 @@ impl Tcb {
         self.send_datagram(dev, tcph, &[])
     }
 
-    pub fn process_estab(
+    pub fn estab(
         &mut self,
         dev: &mut TunDevice,
         seg: etherparse::TcpHeaderSlice,
-    ) -> TcbResult<()> {
+        payload: &[u8],
+    ) -> std::io::Result<()> {
+        // 1) check sequence number
+        // todo!()
+
+        // 2) check the RST bit,
         if seg.rst() {
-            self.send_rst(dev, seg.sequence_number()).unwrap();
-            return Err(ConnectionError::ConnectionReset);
+            return Err(Error::from(ErrorKind::ConnectionReset));
         }
-        if seg.fin() && seg.ack() {
+
+        // 3) check security and precedence
+        // todo!()
+
+        // 4) check the SYN bit,
+        // todo!()
+
+        // 5) check the ACK field
+        if seg.ack() {
+            let seg_ack = seg.acknowledgment_number();
+            if self.snd_una < seg_ack && seg_ack <= self.snd_nxt {
+                println!("removing acknowledged octets from tx...");
+                self.snd_una = seg_ack;
+                // TODO: remove acknowledged seqs from tx_buffer. We don't need to store them
+                // anymore.
+            }
+            // todo!()
+        }
+
+        if seg.psh() && !payload.is_empty() {
+            self.rx_buffer.extend(payload);
+            println!("Received a message: {}", String::from_utf8_lossy(payload));
+            self.rcv_nxt = self.rcv_nxt.wrapping_add(payload.len() as u32);
+            self.send_ack(dev)?
+        }
+
+        if seg.fin() {
+            // Remote socket won't send anymore!
             println!("connection closing");
-            self.send_ack(dev, seg).unwrap();
+
             self.state = State::CloseWait;
-            return Ok(());
-        }
 
-        let seg_ack = seg.acknowledgment_number();
-        if self.snd_una < seg_ack && seg_ack <= self.snd_nxt {
-            self.snd_una = seg_ack;
-        }
-        println!("Ready for data exchange!");
+            self.rcv_nxt = self.rcv_nxt.wrapping_add(1);
 
-        // todo!();
+            self.send_ack(dev)?
+
+            // TODO: FIN implies PUSH for any segment text not yet delivered to the user.
+        }
 
         Ok(())
     }
 
-    pub fn process_close(
+    pub fn close(
         &mut self,
         dev: &mut TunDevice,
         seg: etherparse::TcpHeaderSlice,
-    ) -> TcbResult<()> {
+    ) -> std::io::Result<()> {
         if !seg.rst() {
             match seg.ack() {
-                true => {
-                    self.send_rst(dev, seg.sequence_number()).unwrap();
-                }
-                false => {
-                    self.send_rst_ack(dev, seg).unwrap();
-                }
+                true => self.send_rst(dev, seg.sequence_number())?,
+                false => self.send_rst_ack(dev, seg)?,
             }
         }
         Ok(())
     }
 
-    pub fn send_ack(
-        &mut self,
-        dev: &mut TunDevice,
-        seg: etherparse::TcpHeaderSlice,
-    ) -> std::io::Result<()> {
-        let ws = self.window().expect("idk man");
+    pub fn close_wait(&mut self, dev: &mut TunDevice) -> std::io::Result<()> {
+        Ok(())
+    }
+
+    pub fn send_ack(&mut self, dev: &mut TunDevice) -> std::io::Result<()> {
+        let ws = self.window().unwrap();
         let mut tcph = etherparse::TcpHeader::new(
             self.pair.local.port(),
             self.pair.remote.port(),
             self.snd_nxt,
             ws,
         );
-        tcph.acknowledgment_number = seg.sequence_number() + 1;
+        tcph.acknowledgment_number = self.rcv_nxt;
         tcph.ack = true;
 
         self.send_datagram(dev, tcph, &[])
@@ -343,5 +372,29 @@ impl Tcb {
     pub fn window(&self) -> Option<u16> {
         let ws = self.rx_buffer.capacity() - self.rx_buffer.len();
         ws.try_into().ok()
+    }
+
+    pub fn send_datagram(
+        &self,
+        dev: &mut TunDevice,
+        tcph: etherparse::TcpHeader,
+        payload: &[u8],
+    ) -> std::io::Result<()> {
+        let builder = etherparse::PacketBuilder::ipv4(
+            self.pair.local.ip().octets(),
+            self.pair.remote.ip().octets(),
+            HOP_LIMIT,
+        )
+        .tcp_header(tcph);
+
+        let mut datagram = Vec::<u8>::with_capacity(builder.size(payload.len()));
+
+        match builder.write(&mut datagram, payload) {
+            Ok(_) => dev.write_all(datagram.as_slice()),
+            Err(_) => Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Packet serialization failed",
+            )),
+        }
     }
 }
