207d8f72b427ee60751220c95fff542f4be12872
Andrii Konotop <konotop401@gmail.com>
2025-04-10 16:40:17 +0200

    feat: add second IPv6 listener

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,14 @@ fn handle_stream(mut stream: TcpStream, addr: SocketAddr) -> std::io::Result<()>
     loop {
         match stream.read(&mut buffer) {
             Ok(0) => {
-                println!("Client {} disconnected!", addr);
+                println!("client {} disconnected!", addr);
                 break;
             }
             Ok(n) => {
                 let received = String::from_utf8_lossy(&buffer[..n]);
-                println!("Received some bytes {:?}", received);
-                if let Err(e) = stream.write(b"omg, don't touch it, it's working!") {
-                    eprintln!("Failed to send response, {:?}", e);
+                println!("message received: {:?}", received);
+                if let Err(e) = stream.write(b"extremely important response payload") {
+                    eprintln!("failed to send response, {:?}", e);
                     break;
                 };
             }
@@ -32,19 +32,37 @@ fn handle_stream(mut stream: TcpStream, addr: SocketAddr) -> std::io::Result<()>
 }
 
 fn main() -> std::io::Result<()> {
+    // init logging
+    tracing_subscriber::fmt().with_thread_ids(true).init();
+
+    // create tun device
     let mut dev = device::TunDevice::new().unwrap();
+
+    // create a manager which will store the connections
     let mgr = Arc::new(ConnectionManager::new());
 
+    // lauch the packet loop for processing incoming packets
     let mgr_ref = Arc::clone(&mgr);
-    let _ph = std::thread::spawn(move || {
+    std::thread::spawn(move || {
         if let Err(e) = packet_loop(&mut dev, mgr_ref) {
             eprintln!("error spawning thread: {}", e);
         }
     });
 
-    let addr = "[fd00:dead:beef::5]:8080".parse().unwrap();
-    let listener = TcpListener::bind(addr, mgr.clone()).unwrap();
-    while let Ok((stream, addr)) = listener.accept() {
+    // launch IPv4 listener on port 8080
+    let addr_1 = "10.0.0.9:8080".parse().unwrap();
+    let listener_1 = TcpListener::bind(addr_1, mgr.clone()).unwrap();
+    std::thread::spawn(move || {
+        while let Ok((stream, addr)) = listener_1.accept() {
+            println!("accepted a connection: {addr}");
+            std::thread::spawn(move || handle_stream(stream, addr));
+        }
+    });
+
+    // launch IPv6 listener on port 8081
+    let addr_2 = "[fd00:dead:beef::5]:8081".parse().unwrap();
+    let listener_2 = TcpListener::bind(addr_2, mgr.clone()).unwrap();
+    while let Ok((stream, addr)) = listener_2.accept() {
         println!("accepted a connection: {addr}");
         std::thread::spawn(move || handle_stream(stream, addr));
     }
